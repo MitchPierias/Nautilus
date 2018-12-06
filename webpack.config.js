eccde557bf5be6ea011b93e82a78a84c9228f5d8
Mitch Pierias <mitch@pierias.com>
2018-12-06 17:23:35 +1000

    Supported .env file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const dotenv = require('dotenv');
 const path = require('path');
 const fs = require('fs');
 
@@ -10,45 +11,58 @@ fs.readdirSync('node_modules').filter(function(x) {
 	nodeModules[mod] = 'commonjs ' + mod;
 });
 
-module.exports = {
-	externals: nodeModules,
-	entry: ['./src/index.js'],
-	target: 'node',
-	output: {
-		path: __dirname,
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				exclude: [
-					/node_modules/,
-					/.json?/
-				],
-				loader: 'babel-loader',
-				query: {
-					presets: ['react', 'es2016', 'stage-1']
+module.exports = () => {
+	// Load .env file
+	const env = dotenv.config().parsed;
+	// Reduce the environment variables to a string
+	const envKeys = Object.keys(env).reduce((prev, next) => {
+		prev[`process.env.${next}`] = JSON.stringify(env[next]);
+		return prev;
+	}, {});
+
+	return {
+		externals: nodeModules,
+		entry: ['./src/index.js'],
+		target: 'node',
+		output: {
+			path: __dirname,
+			publicPath: '/',
+			filename: 'bundle.js'
+		},
+		module: {
+			loaders: [
+				{
+					exclude: [
+						/node_modules/,
+						/.json?/
+					],
+					loader: 'babel-loader',
+					query: {
+						presets: ['react', 'es2016', 'stage-1']
+					}
+				}, {
+					test: /\.css$/,
+					loader: 'style-loader'
+				}, {
+					test: /\.css$/,
+					loader: 'css-loader',
+					query: {
+						modules: true,
+						localIdentName: '[name]_[local]_[hash:base64:4]'
+					}
 				}
-			}, {
-				test: /\.css$/,
-				loader: 'style-loader'
-			}, {
-				test: /\.css$/,
-				loader: 'css-loader',
-				query: {
-					modules: true,
-					localIdentName: '[name]_[local]_[hash:base64:4]'
-				}
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.js', '.jsx']
-	},
-	devServer: {
-		historyApiFallback: true,
-		contentBase: './',
-		port: 4172
+			]
+		},
+		plugins: [
+			new webpack.DefinePlugin(envKeys)
+		],
+		resolve: {
+			extensions: ['.js', '.jsx']
+		},
+		devServer: {
+			historyApiFallback: true,
+			contentBase: './',
+			port: 4172
+		}
 	}
 };
