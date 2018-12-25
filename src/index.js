3ce9089854cac8d7b7cbdb12b5e954880ce84003
Mitch Pierias <mitch@pierias.com>
2018-12-25 23:49:51 +1000

    Moved to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './reducers/index';
 // CSS
@@ -12,10 +12,10 @@ import Controllers from './views';
 const App = (
 	<Provider store={store}>
 		<Router basename="/">
-			<div style={{position:"absolute",width:"100%",height:"100%",display:"flex",flexDirection:"column",justifyContent:"flex-start",alignItems:"stretch",alignContent:"stretch",margin:0,padding:0}}>
+			<Fragment>
 				<Route component={Navigation}/>
 				<Controllers/>
-			</div>
+			</Fragment>
 		</Router>
 	</Provider>
 )
